d64b640ccd6cbc9a75dcdaf1c784ae45be9f3227
Luka Baranovic <lukabaranovic366@gmail.com>
2026-02-15 20:22:15 +0100

    final

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import "./globals.css";
-import { Inter, Playfair_Display } from "next/font/google";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
+import ContactModal from "../components/ContactModal";
 import { ModalProvider } from "../context/ModalContext";
-import Modal from "../components/Modal";
+import "./globals.css";
+// Učitavanje fontova (da naslovi budu onako lijepi serifni)
+import { Inter, Playfair_Display } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const playfair = Playfair_Display({
@@ -10,22 +14,29 @@ const playfair = Playfair_Display({
 });
 
 export const metadata = {
-  // PROMJENA: Ključne riječi malim slovima + Tvoj brend na kraju
+  // NOVI NASLOV: Ključne riječi + Tvoj brend
   title: "Najam pagoda, šatora i catering opreme | Dalmacija Najam",
 
+  // NOVI OPIS: Uključuje šatore, visoke stolove i gradove
   description:
     "Vrhunski najam pagoda, šatora, visokih stolova i kompletne catering opreme za vjenčanja i evente. Dostava i montaža u Splitu, Zadru, Šibeniku i na otocima.",
+
+  keywords:
+    "najam pagoda, najam šatora, catering oprema, visoki stolovi, najam inventara, split, zadar, šibenik, dalmacija",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="hr" className="scroll-smooth">
       <body
-        className={`${inter.variable} ${playfair.variable} font-sans antialiased`}
+        className={`bg-gray-50 flex flex-col min-h-screen ${inter.variable} ${playfair.variable} font-sans`}
       >
         <ModalProvider>
-          {children}
-          <Modal />
+          <ScrollToTop />
+          <Navbar />
+          <ContactModal />
+          <main className="flex-grow">{children}</main>
+          <Footer />
         </ModalProvider>
       </body>
     </html>
